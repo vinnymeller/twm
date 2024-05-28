34751dbb244312078294bca82b2f6837752a69c9
Vinny Meller <vinnymeller@proton.me>
2024-05-28 01:39:30 -0400

    add comment in workspace match

--- src/matches.rs
+++ src/matches.rs
@@ -28,7 +28,11 @@ pub fn find_workspaces_in_dir(dir: &str, config: &TwmGlobal, workspaces: &mut Ve
         for workspace_definition in &config.workspace_definitions {
             if path_meets_workspace_conditions(entry.path(), &workspace_definition.conditions) {
                 // just skip the path if it's not valid utf-8 since we can't use it
+                // skip here instead of checking earlier because i don't expect people having a bunch of non-utf8 paths to be common, so defer the check only if we have a match in the first place
                 if let Some(utf8_path) = entry.path().to_str() {
+                    // previously we also stored which workspace type we matched on, but i decided to change it because we only ever need to know the workspace type for the workspace we're opening anyways
+                    // having to re-lookup the workspace type on user selection is surely better than the hashmap we were using before, but better would probably be to just keep track of which WorkspaceDefinition matched here
+                    // main reason I haven't yet is because I'm not entirely sure how to make that work nicely with the fuzzy finders
                     workspaces.push(utf8_path.to_string());
                 }
                 break;
