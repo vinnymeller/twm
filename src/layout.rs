9f3324c9b7561624b64329f15264f4fb2c308182
Vinny Meller <vinnymeller@proton.me>
2024-06-01 22:20:15 -0400

    Add documentation to configuration structs
    Adds documentation to each field in the twm configuration files.
    
    These docs are passed through to the generated JSON schema and thus will
    be made available in editors to users using a compatible language
    server.

--- src/layout.rs
+++ src/layout.rs
@@ -4,8 +4,34 @@ use serde::Deserialize;
 #[derive(Deserialize, Debug, Clone, JsonSchema)]
 #[serde(deny_unknown_fields)]
 pub struct LayoutDefinition {
+    /// Name of the layout.
+    ///
+    /// This is the name that should be referenced in workspace definitions' `default_layout` field.
     pub name: String,
+
+    /// List of layout names that this layout should inherit commands from.
+    ///
+    /// If unset, commands are not inherited from any other layouts.
+    ///
+    /// Commands are inherited in the order they are listed.
+    ///
+    /// Only layouts in the main `twm.yaml` configuration file can be used in this list. There is no way for twm
+    /// to find all of the local layouts that might exist in other workspaces.
+    ///
+    /// This is useful when you want to share complex base layouts that might slightly differ between different types of
+    /// workspaces. For example, you might define a complicated layout with 5 windows and 20 panes, but want to run
+    /// different commands in some panes Python workspaces than in Rust workspaces. You could define the window & pane
+    /// layout in a base layout and inherit from it in your Python and Rust layouts, simply using the `commands` field
+    /// to run the workspace-specific commands for each respective workspace type.
     pub inherits: Option<Vec<String>>,
+
+    /// List of commands to run when a session using this layout is initialized.
+    ///
+    /// If unset, no commands are run when the session is initialized.
+    ///
+    /// Commands defined here are run after commands from inherited layouts.
+    ///
+    /// These commands are passed to the  shell as-is via tmux's `send-keys` command.
     pub commands: Option<Vec<String>>,
 }
 
